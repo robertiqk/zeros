66030f6e5fc94c49ab91e7c0e56fd43afbe1da1b
robertiqk <delux1945@gmail.com>
2018-09-18 17:54:37 +0300

    Exercise 7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let count = 0;
+  while (number > 0) {
+    number /= 5;
+    count += Math.floor(number);
+  }
+  return count;
 }
